ad893cf15ee0786f1dd8fab3fcc2bcba6d55dd1d
mikeperry <mikeperry-git@fscked.org>
2009-06-21 02:43:55 +0000

    Fix cron script to save backups of scan results.
    git-svn-id: https://svn.torproject.org/svn/torflow/trunk@19750 55e972cd-5a19-0410-ae62-a4d7a52db4cd

--- NetworkScanners/BwAuthority/cron.sh
+++ NetworkScanners/BwAuthority/cron.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+TIMESTAMP=`date +%Y%m%d-%H%M`
 SCANNER_DIR=~/code/torflow-trunk/NetworkScanners/BwAuthority
 TOR_DEST=$SCANNER_DIR/bwscan.V3BandwidthsFile
+OUTPUT=$SCANNER_DIR/data/bwscan.${TIMESTAMP}
 
 cd $SCANNER_DIR # Needed for import to work properly.
-$SCANNER_DIR/aggregate.py $SCANNER_DIR/data $SCANNER_DIR/data/bwscan.all
+$SCANNER_DIR/aggregate.py $SCANNER_DIR/data $OUTPUT
 
-cp $SCANNER_DIR/data/bwscan.all $TOR_DEST
-#scp $SCANNER_DIR/data/bwscan.all tor@authority.org:/var/lib/tor/V3Bandwidths 
+cp $OUTPUT $TOR_DEST
+#scp $OUTPUT tor@authority.org:/var/lib/tor/V3Bandwidths 
 
