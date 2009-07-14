c79796ad616136084df64213ed7aa5f92c5e470b
mikeperry <mikeperry-git@fscked.org>
2009-07-14 05:56:30 +0000

    Make run_scan.sh kill old tors and scanners, and make it renice the bwauthority.py's to 20.
    git-svn-id: https://svn.torproject.org/svn/torflow/trunk@20002 55e972cd-5a19-0410-ae62-a4d7a52db4cd

--- NetworkScanners/BwAuthority/run_scan.sh
+++ NetworkScanners/BwAuthority/run_scan.sh
@@ -4,20 +4,19 @@
 # Ie: 
 #      git remote add mikeperry git://git.torproject.org/~mikeperry/git/tor
 #      git fetch mikeperry
-#      git branch --track rs-format-fix mikeperry/rs-format-fix
-#      git checkout rs-format-fix
+#      git branch --track rs-format-fix7 mikeperry/rs-format-fix7
+#      git checkout rs-format-fix7
 TOR_EXE=../../../tor.git/src/or/tor
 PYTHONPATH=../../../SQLAlchemy-0.5.5/lib:../../../Elixir-0.6.1/
+
+# NOTE: You may want to remove this line if these are not the only
+# tors run by this user:
+killall tor && killall bwauthority.py && sleep 5 && killall -9 tor
 
 for i in data/scanner.*
 do
   rm $i/scan-data/*
 done
-
-# NOTE: You may want to add this step, if these are the only
-# tors run by this user.
-# killall tor && sleep 5 && killall -9 tor
-
 
 $TOR_EXE -f ./data/scanner.1/torrc & 
 $TOR_EXE -f ./data/scanner.2/torrc & 
@@ -30,9 +29,9 @@ sleep 60
 
 export PYTHONPATH
 
-./bwauthority.py ./data/scanner.1/bwauthority.cfg >& ./data/scanner.1/bw.log &
-./bwauthority.py ./data/scanner.2/bwauthority.cfg >& ./data/scanner.2/bw.log &
-./bwauthority.py ./data/scanner.3/bwauthority.cfg >& ./data/scanner.3/bw.log &
-./bwauthority.py ./data/scanner.4/bwauthority.cfg >& ./data/scanner.4/bw.log &
+nice -n 20 ./bwauthority.py ./data/scanner.1/bwauthority.cfg >& ./data/scanner.1/bw.log &
+nice -n 20 ./bwauthority.py ./data/scanner.2/bwauthority.cfg >& ./data/scanner.2/bw.log &
+nice -n 20 ./bwauthority.py ./data/scanner.3/bwauthority.cfg >& ./data/scanner.3/bw.log &
+nice -n 20 ./bwauthority.py ./data/scanner.4/bwauthority.cfg >& ./data/scanner.4/bw.log &
 
 
