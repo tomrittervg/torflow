fc8c1c99e67785c6cfb52fa922473865ef0d80d3
mikeperry <mikeperry-git@fscked.org>
2009-09-23 01:27:05 +0000

    Make everything slightly more informative.
    git-svn-id: https://svn.torproject.org/svn/torflow/trunk@20652 55e972cd-5a19-0410-ae62-a4d7a52db4cd

--- CircuitAnalysis/BuildTimes/pareto.py
+++ CircuitAnalysis/BuildTimes/pareto.py
@@ -8,22 +8,34 @@
 # Note that alpha is not the only potential source of estimate error though.
 # With insufficient samples we may not have a valid value for Xm either.
 # I'm uncertain at this point on how to account for this or bound this error.
+#
+# One other thing to note is the possible interplay with the CDF point
+# and the values from binomial.py
 import math
 
 def CalculateTimeout(Xm, alpha, q):
   return (Xm/math.pow(1.0-q,1.0/alpha))/1000.0;
 
-sample_sizes = [100, 150, 200, 250, 300, 350, 400, 500, 600, 800, 1000]
+def ParetoF(Xm, alpha, timeout):
+  return math.pow(Xm/timeout, alpha)
+
+sample_sizes = [50, 100, 250, 500, 1000, 2500, 5000]
 
 def dump_table(Xm, alpha):
   for n in sample_sizes:
     sigma = alpha/math.sqrt(n)
-    print "Timeouts for Xm: "+str(Xm)+", alpha: "+str(alpha)+" n:"+str(n)+" sigma: "+str(sigma)
-    print "\t -2*sigma: "+str(CalculateTimeout(Xm, alpha-2*sigma, .8))
-    print "\t -1*sigma: "+str(CalculateTimeout(Xm, alpha-sigma, .8))
-    print "\t  0*sigma: "+str(CalculateTimeout(Xm, alpha, .8))
-    print "\t +1*sigma: "+str(CalculateTimeout(Xm, alpha+1*sigma, .8))
-    print "\t +2*sigma: "+str(CalculateTimeout(Xm, alpha+2*sigma, .8))
+    print "Timeouts for Xm: "+str(Xm)+", alpha: "+str(alpha)+" n="+str(n)+" sigma: "+str(sigma)
+    t=CalculateTimeout(Xm, alpha-2*sigma, .8)
+    print "\t -2*sigma: "+str(round(t,1))+" timeout %="+str(round(100*ParetoF(Xm, alpha, t*1000),1))
+    t=CalculateTimeout(Xm, alpha-1*sigma, .8)
+    print "\t -1*sigma: "+str(round(t,1))+" timeout %="+str(round(100*ParetoF(Xm, alpha, t*1000),1))
+    t=CalculateTimeout(Xm, alpha, .8)
+    print "\t  0*sigma: "+str(round(t,1))+" timeout %="+str(round(100*ParetoF(Xm, alpha, t*1000),1))
+    t=CalculateTimeout(Xm, alpha+1*sigma, .8)
+    print "\t +1*sigma: "+str(round(t,1))+" timeout %="+str(round(100*ParetoF(Xm, alpha, t*1000),1))
+    t=CalculateTimeout(Xm, alpha+2*sigma, .8)
+    print "\t +2*sigma: "+str(round(t,1))+" timeout %="+str(round(100*ParetoF(Xm, alpha, t*1000),1))
+
 
 dump_table(1950, 1.4)
 print
