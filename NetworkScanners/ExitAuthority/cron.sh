8543600a4af4ff460cc69ef4993a0ed2860d8354
Mike Perry <mikeperry-git@fscked.org>
2010-10-06 16:28:50 -0700

    Fix CWD issues with BeautifulSoup and other libs.

--- NetworkScanners/ExitAuthority/cron.sh
+++ NetworkScanners/ExitAuthority/cron.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SCANDIR=~/code/torflow.git/NetworkScanners/ExitAuthority/
+cd $SCANDIR
 
 # 1. Email results to addresses in soat_config.py (--email)
 # 2. Ignore timeout errors (--noreason FailureTimeout)
@@ -8,10 +9,10 @@ SCANDIR=~/code/torflow.git/NetworkScanners/ExitAuthority/
 # 4. Only report from urls that fail from less than 10% of the total
 #    exits tested so far. (--siterate 10)
 # 5. Only report exits that fail 100% of their tests (--exitrate 99)
-$SCANDIR/snakeinspector.py --email --exitrate 99 --siterate 10 --croninterval 1
+./snakeinspector.py --email --exitrate 99 --siterate 10 --croninterval 1
 
 # Optionally, you can use these two lines to allow less regular cron
 # scheduling:
 
-#$SCANDIR/snakeinspector.py --confirmed --email --noreason FailureTimeout --siterate 3 --finishedafter "`cat $SCANDIR/lastmail.time`"
+#./snakeinspector.py --confirmed --email --noreason FailureTimeout --siterate 3 --finishedafter "`cat $SCANDIR/lastmail.time`"
 #date +"%a %b %d %H:%M:%S %Y" > $SCANDIR/lastmail.time
