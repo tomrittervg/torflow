f964c66e4eeae5c6d21c540d9439573a0cb0f968
Mike Perry <mikeperry-git@fscked.org>
2011-11-04 15:05:19 -0700

    Update cron script to differentiate output vs archive.

--- NetworkScanners/BwAuthority/cron.sh
+++ NetworkScanners/BwAuthority/cron.sh
@@ -3,14 +3,14 @@
 SCANNER_DIR=~/code/tor/torflow/NetworkScanners/BwAuthority
 
 TIMESTAMP=`date +%Y%m%d-%H%M`
-TOR_DEST=$SCANNER_DIR/bwscan.V3BandwidthsFile
-OUTPUT=$SCANNER_DIR/data/bwscan.${TIMESTAMP}
+ARCHIVE=$SCANNER_DIR/data/bwscan.${TIMESTAMP}
+OUTPUT=$SCANNER_DIR/bwscan.V3BandwidthsFile
 
 cd $SCANNER_DIR # Needed for import to work properly.
 $SCANNER_DIR/aggregate.py $SCANNER_DIR/data $OUTPUT
 
 if [ $? = 0 ]
 then
- cp $OUTPUT $TOR_DEST
- #scp $TOR_DEST bwscan@torauthority.org:/var/lib/tor.scans/bwscan > /dev/null
+ cp $OUTPUT $ARCHIVE
+ #scp $OUTPUT bwscan@torauthority.org:/var/lib/tor.scans/bwscan > /dev/null
 fi
